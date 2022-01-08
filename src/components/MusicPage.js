bd6f6a1491dbee451a687090022f0cc972510727
Derek Araujo <derekaraujo19@gmail.com>
2022-01-08 00:14:13 -0500

    Built structure for first music fetch

--- src/components/MusicPage.js
+++ src/components/MusicPage.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+// useEffect function that makes a get request to the db.json server on load and stores data in state
+// send that information down to a MusicCard component via a map
+// don't forget to check/rename the api endpoints
 
 function MusicPage() {
+  const [music, setMusic] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((r) => r.json())
+      .then((musicItems) => setMusic(musicItems));
+  }, []);
+
+  console.log(music);
+
+  // Now, take "music" and map it out and send down to MusicCard component
+
   return (
     <div>
       <h3>MUSIC PAGE WILL GO HERE</h3>
