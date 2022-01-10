c79441af4705848136927ae2aff909c1d2046589
Derek Araujo <derekaraujo19@gmail.com>
2022-01-10 12:01:06 -0500

    working music page functionality

--- src/components/MusicPage.js
+++ src/components/MusicPage.js
@@ -1,25 +1,33 @@
 import React, { useState, useEffect } from "react";
+import MusicCard from "./MusicCard";
 
 // useEffect function that makes a get request to the db.json server on load and stores data in state
 // send that information down to a MusicCard component via a map
 // don't forget to check/rename the api endpoints
 
 function MusicPage() {
-  const [music, setMusic] = useState([]);
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:3001/toys")
       .then((r) => r.json())
-      .then((musicItems) => setMusic(musicItems));
+      .then((musicItems) => setProjects(musicItems));
   }, []);
 
-  console.log(music);
+  console.log(projects);
 
   // Now, take "music" and map it out and send down to MusicCard component
 
   return (
     <div>
-      <h3>MUSIC PAGE WILL GO HERE</h3>
+      <ul className="cards">
+        {projects.map((project) => (
+          <MusicCard
+          key={project.id}
+          title={project.name}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
